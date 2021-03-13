6fc74b932639937cecf1b6bb6e0fc0d68d8a13f7
Carlos Wosiak <carlosw403@hotmail.com>
2021-03-13 18:17:49 -0300

    delete unused file

--- mongo.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://mongodb:27017/blog', {useNewUrlParser: true, useUnifiedTopology: true});
-
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-mongoose.connection.once('open', function() { console.log('mongoose connected')});
